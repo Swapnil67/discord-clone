9819396fde70a3972c452b3464515f87073724cb
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-01-19 00:28:19 +0530

    Added Clerk and did basic authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <>
       <p className='text-3xl font-bold text-indigo-500'>This is a protected route</p>
+      <UserButton afterSignOutUrl="/" />
     </>
   )
 }
