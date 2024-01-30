6bfd0d9da680a8ed3fac4abfa1ea6fa37641a9fd
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-01-30 23:18:36 +0530

    Working on manage mebers

---
+++ components/modals/members-modal.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React, { useState } from "react";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogHeader,
+  DialogContent,
+  DialogDescription,
+} from "@/components/ui/dialog";
+
+import { useModal } from "@/hooks/use-modal-store";
+import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import UserAvatar from "@/components/user-avatar";
+import { ShieldCheck } from "lucide-react";
+
+const roleIconMap = {
+  GUEST: null,
+  ADMIN: <ShieldCheck className="h-4 w-4 ml-3 text-rose-500" />,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-3 text-indigo-500" />
+}
+
+const MembersModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "members";
+  const { server } = data as { server: ServerWithMembersWithProfiles };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Manage Members
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} Members
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => {
+            return (
+              <div key={member.id} className="flex items-center gap-x-2 mb-6">
+                <UserAvatar src={member.profile.imageUrl} />
+                <div className="flex flex-col gay-y-1">
+                  <div className="text-xs font-semibold flex items-center gap-x-1">
+                    {member.profile.name}
+                    {roleIconMap[member.role]}
+                  </div>
+                  <p className="text-xs text-zinc-500">{member.profile.email}</p>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MembersModal;
