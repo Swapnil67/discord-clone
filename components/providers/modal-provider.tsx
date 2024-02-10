f142f0462128ebc60d4c348cf66728f1f0444148
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-02-10 11:29:07 +0530

    Attachment chat feature

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,7 +10,7 @@ import CreateChannelModal from "@/components/modals/create-channel-modal";
 import DeleteServerModal from "@/components/modals/delete-server-modal";
 import DeleteChannelModal from "@/components/modals/delete-channel-modal";
 import EditChannelModal from "@/components/modals/edit-channel-modal";
-import MessageFileModal from "../modals/message-file-modal";
+import MessageFileModal from "@/components/modals/message-file-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
